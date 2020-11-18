704dc486267b98d01213600255a605bfeca54f44
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2020-11-18 19:32:37 +1100

    Minor docstring and code formatting fixes
    References:
    
    - https://numpy.org/doc/stable/docs/howto_document.html
    - https://www.python.org/dev/peps/pep-0008/
    - https://www.python.org/dev/peps/pep-0257/
    
    Note: the NumPy documentation format is a refinement of
    PEP257, ie, it adheres to PEP257 and adds more conventions.

--- RHaP/import_image.py
+++ RHaP/import_image.py
@@ -2,29 +2,29 @@ import numpy as np
 from skimage.color import rgb2gray
 from skimage.io import imread
 
+
 def image(image_path, transpose):
-    """
-    Import and Transpose an image.
+    """Import and transpose an image.
 
     Parameters
     ----------
-        image_path: str
-            Path to the image, excluding extension, including name
-        transpose: Boolean Value
-            Whether the image needs to be rotated so that radial hydrides are in the vertical direction
-            If True: Image will be transposed
-            If False: Image will not be transposed
+    image_path: str
+        Path to the image, excluding extension, including name
+    transpose: bool
+        Whether the image needs to be rotated so that radial hydrides are in
+        the vertical direction. If True, the read array will be transposed
+        before returning.
 
     Returns
     -------
-        original_image
-            The transposed/imported image to be analysed
+    original_image
+        The transposed/imported image to be analysed
     """
 
     original_image = imread(image_path)
     original_image = rgb2gray(original_image)
 
-    if transpose == True:
+    if transpose:
         original_image = np.transpose(original_image)
 
     return original_image
